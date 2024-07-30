08f165239ecf31fc83270f3a9e4889d26aa437c5
Deepak-Das-7 <deepak.d@feiniceenergy.com>
2024-07-31 00:24:02 +0530

    improved admin panel in the form of profile211

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
+const { getPortfolioItems, createPortfolioItem, editPortfolioItem, deletePortfolioItem } = require('../controllers/PortfolioController');
 const { getPhoto, createPhoto } = require('../controllers/Photo');
-const { getPortfolioItems, createPortfolioItem, deletePortfolioItem, editPortfolioItem } = require("../controllers/PortfolioController")
-
 
 const userController = require("../controllers/UserController");
 const PostController = require("../controllers/PostController");
@@ -12,46 +11,46 @@ const TagController = require("../controllers/TagController");
 const router = Router();
 
 // Portfolio routes
-router.get('/portfolio', getPortfolioItems);
-router.post('/portfolio', createPortfolioItem);
-router.put('/portfolio/:id', editPortfolioItem);
-router.put('/portfolio/delete/:id', deletePortfolioItem);
+router.get('/', getPortfolioItems);
+router.post('/', createPortfolioItem);
+router.put('/edit/:id', editPortfolioItem);
+router.put('/delete/:id', deletePortfolioItem);
 
 // Photo routes
 router.get('/photo', getPhoto);
 router.post('/upload', createPhoto);
 
 // User routes
-router.get('/users', userController.getAllUsers);
 router.post('/users', userController.createUser);
+router.get('/users', userController.getAllUsers);
 router.get('/users/:id', userController.getUserById);
 router.put('/users/:id', userController.updateUserById);
 router.delete('/users/:id', userController.deleteUserById);
 
 // Post routes
-router.get('/posts', PostController.getAllPosts);
 router.post('/posts', PostController.createPost);
+router.get('/posts', PostController.getAllPosts);
 router.get('/posts/:id', PostController.getPostById);
 router.put('/posts/:id', PostController.updatePostById);
 router.delete('/posts/:id', PostController.deletePostById);
 
 // Comment routes
-router.get('/comments', CommentController.getAllComments);
 router.post('/comments', CommentController.createComment);
+router.get('/comments', CommentController.getAllComments);
 router.get('/comments/:id', CommentController.getCommentById);
 router.put('/comments/:id', CommentController.updateCommentById);
 router.delete('/comments/:id', CommentController.deleteCommentById);
 
 // Category routes
-router.get('/categories', CategoryController.getAllCategories);
 router.post('/categories', CategoryController.createCategory);
+router.get('/categories', CategoryController.getAllCategories);
 router.get('/categories/:id', CategoryController.getCategoryById);
 router.put('/categories/:id', CategoryController.updateCategoryById);
 router.delete('/categories/:id', CategoryController.deleteCategoryById);
 
 // Tag routes
-router.get('/tags', TagController.getAllTags);
 router.post('/tags', TagController.createTag);
+router.get('/tags', TagController.getAllTags);
 router.get('/tags/:id', TagController.getTagById);
 router.put('/tags/:id', TagController.updateTagById);
 router.delete('/tags/:id', TagController.deleteTagById);
